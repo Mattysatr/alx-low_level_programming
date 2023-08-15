a3db6d81fcd5025a31c4dea5741a418d31954bc9
Collins <Mattysatr>
2023-08-15 03:07:09 -0700

    0x02

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+i#include <stdio.h>
+
+/**
+* print_putchar - Fuction to print _putcher followed by a new line
+*
+* This function prints each character of the word "_putchar"
+* followed by a newline character using the _putchar function.
+*/
+
+print_putcher(void)
+{
+/*calling _putcher to print each character*/
+_putchar('_');
+_putchar('p');
+_putchar('u);
+_putchar('t);
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+
+/* Printing a newline character */
+_putchar('\n');
+}
