020efbee425cb0419430e4f51f05ba1b3c3f412e
collins tobore <mattstallin1@gmail.com>
2023-09-05 21:35:29 +0000

    malloc

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -8,14 +8,14 @@
  * Description: frees grid memory
  * Return: nothing
  *
- *
  */
 
-void free_grid(int **grid, int height);
+void free_grid(int **grid, int height)
 {
-int i;
+if (grid == NULL)
+return;
 
-for (i = 0; i < height; i++)
+for (int i = 0; i < height; i++)
 {
 free(grid[i]);
 }
